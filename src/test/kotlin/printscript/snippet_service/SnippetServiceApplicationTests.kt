e0295af1c5f29775e9c08482abda70385233d202
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2025-09-30 13:09:47 -0300

    build.gradle formatter, linter, coverage y tests

--- src/test/kotlin/printscript/snippet_service/SnippetServiceApplicationTests.kt
+++ src/test/kotlin/printscript/snippet_service/SnippetServiceApplicationTests.kt
@@ -1,13 +1,13 @@
-//package printscript.snippet_service
-//
-//import org.junit.jupiter.api.Test
-//import org.springframework.boot.test.context.SpringBootTest
-//
-//@SpringBootTest
-//class SnippetServiceApplicationTests {
-//
-//	@Test
-//	fun contextLoads() {
-//	}
-//
-//}
+package printscript.snippet_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
